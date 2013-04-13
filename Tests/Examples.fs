de906020ef49286e70bb96f4fb7a5562e390dbd5
ptrelford <ptrelford@localhost>
2013-04-13 23:05:14 +0100

    F# Quotation to IL compiler early prototype

---
+++ Tests/Examples.fs
@@ -0,0 +1,11 @@
+﻿[<ReflectedDefinition>]
+module Examples
+
+let ``1`` () = 1
+let ``1 + 1`` () = 1 + 1
+let ``1 + 1 = 2`` () = 1 + 1 = 2
+let ``1 < 2`` () = 1 < 2
+let ``1 > 2`` () = 1 > 2
+let ``2 <= 2`` () = 2 <= 2
+let ``Max`` () = System.Math.Max(0,1)
+let ``Array`` () = [|1;2;3|]
---
+++ Tests/Tests.fs
@@ -0,0 +1,28 @@
+﻿module Tests
+
+open Fil
+open System.Reflection
+open Microsoft.FSharp.Quotations
+open Microsoft.FSharp.Quotations.Patterns
+open NUnit.Framework
+
+[<TestCaseSource("examples")>]
+let test (methodInfo:MethodInfo) =
+    let expr =
+        match Expr.TryGetReflectedDefinition(methodInfo) with
+        | Some(Lambda(_,expr)) -> expr
+        | Some(_) -> failwith "expecting lambda"
+        | None -> invalidOp ""
+    let f = compileUntyped methodInfo.ReturnType expr
+    let expected = methodInfo.Invoke(null, [||])
+    let actual = f ()
+    Assert.AreEqual(expected, actual)
+
+type internal Marker = interface end 
+let examples () = seq {
+    for t in typeof<Marker>.Assembly.GetTypes() do
+        for methodInfo in t.GetMethods() do
+            match Expr.TryGetReflectedDefinition(methodInfo) with
+            | Some(expr) -> yield methodInfo
+            | _ -> ()
+    }
