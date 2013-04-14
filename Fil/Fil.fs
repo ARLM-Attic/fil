0aed41150e1cfbd9dbb420d2758156ccca638f16
ptrelford <ptrelford@localhost>
2013-04-14 11:32:11 +0100

    local let support

--- Fil/Fil.fs
+++ Fil/Fil.fs
@@ -27,45 +27,51 @@ let (|String|_|) = function
     | Value(v,t) when t = typeof<string> -> Some(v :?> string)
     | _ -> None
 
-let rec generate (il:ILGenerator) = function
+let rec generate env (il:ILGenerator) = function
     | Int v -> generateInt il v
     | Int64 v  -> il.Emit(OpCodes.Ldc_I8, v)
     | Float v -> il.Emit(OpCodes.Ldc_R8, v)
     | Float32 v -> il.Emit(OpCodes.Ldc_R4, v)
-    | Byte v -> generateInt il (int v) //il.Emit(OpCodes.Conv_U1)
-    | Char v -> generateInt il (int v) //il.Emit(OpCodes.Conv_U2)
+    | Byte v -> generateInt il (int v)
+    | Char v -> generateInt il (int v)
     | String v -> il.Emit(OpCodes.Ldstr, v)
-    | NewArray(t,args) -> generateArray il t args
+    | NewArray(t,args) -> generateArray env il t args
     | SpecificCall <@@ (+) @@> (None, _, [Int l;Int r]) -> generateInt il (l+r)
-    | SpecificCall <@@ (+) @@> (None, _, args) -> generateOps il args [OpCodes.Add]        
-    | SpecificCall <@@ (-) @@> (None, _, args) -> generateOps il args [OpCodes.Sub]
-    | SpecificCall <@@ (*) @@> (None, _, args) -> generateOps il args [OpCodes.Mul]
-    | SpecificCall <@@ (/) @@> (None, _, args) -> generateOps il args [OpCodes.Div]
-    | SpecificCall <@@ (=) @@> (None, _, args) -> generateOps il args [OpCodes.Ceq]
-    | SpecificCall <@@ (<>) @@> (None, _, args) -> generateOps il args [OpCodes.Ceq;OpCodes.Ldc_I4_0;OpCodes.Ceq]
-    | SpecificCall <@@ (<) @@> (None, _, args) -> generateOps il args [OpCodes.Clt]
-    | SpecificCall <@@ (<=) @@> (None, _, args) -> generateOps il args [OpCodes.Cgt;OpCodes.Ldc_I4_0;OpCodes.Ceq]
-    | SpecificCall <@@ (>) @@> (None, _, args) -> generateOps il args [OpCodes.Cgt]
-    | SpecificCall <@@ (>=) @@> (None, _, args) -> generateOps il args [OpCodes.Clt;OpCodes.Ldc_I4_0;OpCodes.Ceq]
+    | SpecificCall <@@ (+) @@> (None, _, args) -> generateOps env il args [OpCodes.Add]        
+    | SpecificCall <@@ (-) @@> (None, _, args) -> generateOps env il args [OpCodes.Sub]
+    | SpecificCall <@@ (*) @@> (None, _, args) -> generateOps env il args [OpCodes.Mul]
+    | SpecificCall <@@ (/) @@> (None, _, args) -> generateOps env il args [OpCodes.Div]
+    | SpecificCall <@@ (=) @@> (None, _, args) -> generateOps env il args [OpCodes.Ceq]
+    | SpecificCall <@@ (<>) @@> (None, _, args) -> generateOps env il args [OpCodes.Ceq;OpCodes.Ldc_I4_0;OpCodes.Ceq]
+    | SpecificCall <@@ (<) @@> (None, _, args) -> generateOps env il args [OpCodes.Clt]
+    | SpecificCall <@@ (<=) @@> (None, _, args) -> generateOps env il args [OpCodes.Cgt;OpCodes.Ldc_I4_0;OpCodes.Ceq]
+    | SpecificCall <@@ (>) @@> (None, _, args) -> generateOps env il args [OpCodes.Cgt]
+    | SpecificCall <@@ (>=) @@> (None, _, args) -> generateOps env il args [OpCodes.Clt;OpCodes.Ldc_I4_0;OpCodes.Ceq]
     | Call(None,mi,args) -> 
-        generateAll il args
+        generateAll env il args
         il.EmitCall(OpCodes.Call, mi, null)
+    | Let(var, assignment, cont) ->
+        let local = il.DeclareLocal(var.Type)
+        generate env il assignment
+        il.Emit(OpCodes.Stloc, local)
+        let env = (var.Name,local)::env
+        generate env il cont
+    | Var(var) ->
+        match env |> List.tryFind (fst >> (=) var.Name) with
+        | Some(_, local) -> il.Emit(OpCodes.Ldloc, local.LocalIndex)
+        | None -> invalidOp ""
     | arg -> raise <| System.NotSupportedException(arg.ToString())
-and generateArray (il:ILGenerator) t args =
+and generateArray env (il:ILGenerator) t args =
     generateInt il args.Length
     il.Emit(OpCodes.Newarr,t)
     args |> Seq.iteri (fun i arg ->
         il.Emit(OpCodes.Dup)
         generateInt il i
-        generate il arg
-        generateArrayStoreElem il t
+        generate env il arg
+        il.Emit(OpCodes.Stelem,t)
     )
-and generateArrayStoreElem il t =
-    match t with
-    | _ when t = typeof<int> -> il.Emit(OpCodes.Stelem_I4)
-    | _ -> raise <| System.NotImplementedException()
-and generateOps (il:ILGenerator) args ops =
-    generateAll il args
+and generateOps env (il:ILGenerator) args ops =
+    generateAll env il args
     for op in ops do il.Emit(op)
 and generateInt (il:ILGenerator) = function
     | 0 -> il.Emit(OpCodes.Ldc_I4_0)
@@ -79,13 +85,13 @@ and generateInt (il:ILGenerator) = function
     | 8 -> il.Emit(OpCodes.Ldc_I4_8)
     | s when s >= -127 && s <= 128 -> il.Emit(OpCodes.Ldc_I4_S, s) 
     | n -> il.Emit(OpCodes.Ldc_I4, n)
-and generateAll il args = for arg in args do generate il arg
+and generateAll env il args = for arg in args do generate env il arg
 
 type internal Marker = interface end
 let compileUntyped t (quotation:Expr) =
     let f = DynamicMethod("f", t, [||], typeof<Marker>.Module)
     let il = f.GetILGenerator()
-    quotation |> generate il
+    quotation |> generate [] il
     il.Emit(OpCodes.Ret)
     fun () -> f.Invoke(null,[||])
 
